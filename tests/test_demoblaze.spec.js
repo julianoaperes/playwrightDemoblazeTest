f53175918827adb121f6cc68dde75c10a7c7be42
Juliano Peres <juliano.a.peres@gmail.com>
2024-05-26 11:45:06 -0300

    login locator

--- tests/test_demoblaze.spec.js
+++ tests/test_demoblaze.spec.js
@@ -2,8 +2,13 @@ import{test,expect} from "@playwright/test"
 
 // Login into the Demoblaze https://www.demoblaze.com/
 test('Login', async ({page})=> {
-
+    //Accessing the page
     await page.goto('https://www.demoblaze.com/');
-    
+    //Locating the login field by its PROPERTY 
+    await page.locatork('id=login2').click();
+    // Filling the login INPUT BOX using CSS and SELECTORSHUB to provide the locator '#loginusername'
+    await page.locator('#loginusername').fill('juliano.a.peres@gmail.com');
 
+
+    
 })
