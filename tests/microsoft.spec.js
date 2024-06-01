a5e1d376198eea1de252bbe9f272aae3cdb2173e
Juliano Peres <juliano.a.peres@gmail.com>
2024-06-01 09:41:47 -0300

    hover an span

---
+++ tests/microsoft.spec.js
@@ -0,0 +1,34 @@
+import{test,expect} from "@playwright/test"
+
+// Login into the Demoblaze https://www.demoblaze.com/
+test('Login', async ({page})=> {
+       //Accessing the page
+    await page.goto('https://login.microsoftonline.com/');
+       //Locating the login BUTTON field by its PROPERTY and click on it
+    await page.locator('id=i0116').click();
+       // Filling the login INPUT BOX using CSS and SELECTORSHUB to provide the locator '#loginusername'
+    await page.locator('#i0116').fill("juliano.peres-ext@ab-inbev.com");
+
+    await page.locator("#idSIButton9").click();
+       // Providing the password by CSS and SELECTORHUB 
+    await page.locator("//input[@id='i0118']").fill("Ja!Pe#res@19$86_");
+       // Clicking on the "login" button using CSS
+    await page.locator("#idSIButton9").click(); 
+
+    await page.locator("#idSIButton9").click();
+
+    await page.goto('https://tsc-qa.ab-inbev.com/#/mid-year-review/requests');
+
+     await page.hover('div.c-dUEiTH > a > li.c-dUEiTH.c-cCFkOr.c-cCFkOr-dAlmDS-active-true.c-cCFkOr-IaaiB-isTriggerItem-true.c-dUEiTH-ihdDVLc-css');
+
+     await page.waitForSelector('data-radis-focus-guard tabindex=0 style=outline: none; opacity 0; position: fixed; pointer-events: none', {state: 'visible'});
+     
+     await page.click('data-radis-focus-guard tabindex=0 style=outline: none; opacity 0; position: fixed; pointer-events: none');
+
+
+});
+   
+    //await page.locator("button[title='Actions']").click();
+   // await page.locator("//span[@class='ant-typography c-cMILyz']").click();
+  
+
--- tests/test_demoblaze.spec.js
+++ tests/test_demoblaze.spec.js
@@ -2,12 +2,24 @@ import{test,expect} from "@playwright/test"
 
 // Login into the Demoblaze https://www.demoblaze.com/
 test('Login', async ({page})=> {
-    //Accessing the page
+       //Accessing the page
     await page.goto('https://www.demoblaze.com/');
-    //Locating the login field by its PROPERTY 
-    await page.locatork('id=login2').click();
-    // Filling the login INPUT BOX using CSS and SELECTORSHUB to provide the locator '#loginusername'
-    await page.locator('#loginusername').fill('juliano.a.peres@gmail.com');
+       //Locating the login BUTTON field by its PROPERTY and click on it
+    await page.locator('id=login2').click();
+       // Filling the login INPUT BOX using CSS and SELECTORSHUB to provide the locator '#loginusername'
+    await page.locator('#loginusername').fill("juliano.a.peres@gmail.com");
+       // Providing the password by CSS and SELECTORHUB 
+    await page.locator("input[id='loginpassword']").fill("Ju@1986_");
+       // Clicking on the "login" button using CSS
+    await page.locator("//button[normalize-space()='Log in']").click(); 
+       // Verify logout link 
+   // const logoutlink = await page.locator("//a[normalize-space()=Log out']").click();
+
+     //await expect(logoutlink).toBeVisible();
+
+
+
+
 
 
     
